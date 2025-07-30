76da36a2a0716f26b6d0c20ae75889a1eca7af15
Allan_Maldonado <allansanm@gmail.com>
2025-07-30 11:26:22 -0300

    fix: fixing the users table

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,5 +1,5 @@
 import { reset, seed } from 'drizzle-seed'
-import { db, sql } from '../config/database.ts'
+import { db, sql } from './connection.ts'
 import { schema } from './index.ts'
 
 await reset(db, schema)
@@ -11,7 +11,7 @@ await seed(db, schema).refine((f) => {
       columns: {
         name: f.fullName(),
         email: f.email(),
-        password_hash: f.string()
+        password: f.string()
       },
     }, 
   }
